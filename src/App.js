61556531f9c988ef3cad142e1470eb1e4926cf0d
Mitzi17 <m_estrada17@live.com>
2021-06-29 21:27:37 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        APP
+      </div>
+      )
+    }
+  }
+
+
+
+export default App;
