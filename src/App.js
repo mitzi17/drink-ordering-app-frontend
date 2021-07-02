a371163f98ff40561ede12d85ce78605c41349fa
Mitzi17 <m_estrada17@live.com>
2021-07-02 00:37:18 -0700

    render OrdersContainer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-
-import './App.css';
 import React from 'react';
+import {connect} from 'react-redux'
+import OrdersContainer from './containers/OrdersContainer'
+
 
 class App extends React.Component {
+
+  componentDidMount() {
+    
+  }
 
   render () {
     return (
       <div className="App">
-        APP
+        <OrdersContainer/>
       </div>
       )
     }
   }
 
 
-
-export default App;
+export default connect()(App);
