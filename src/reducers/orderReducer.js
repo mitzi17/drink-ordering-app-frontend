df7e9923082a02d8c9cbdc68cb0e6f9d926b0a58
Mitzi17 <m_estrada17@live.com>
2021-07-04 12:39:53 -0700

    add create_order case

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -3,6 +3,8 @@ export default function orderReducer( state= { orders: [] }, action ) {
     switch (action.type) {
         case 'FETCH_ORDERS':
             return {orders: action.payload}
+        case 'CREATE_ORDER':
+            return {...state, orders: [...state.orders, action.payload]}
         default:
             return state
     }
