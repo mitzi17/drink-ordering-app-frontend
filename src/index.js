b6bd702b3bd242d4a36b34729d2204feb070de03
Mitzi17 <m_estrada17@live.com>
2021-07-08 00:21:41 -0700

    import BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import orderReducer from './reducers/orderReducer';
+import orderReducer from './reducers/orderReducer'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
@@ -12,7 +13,9 @@ let store = createStore(orderReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
