2b2fcd975ac45c4f531ac28f3529089105ecef0e
Mitzi17 <m_estrada17@live.com>
2021-07-09 11:45:23 -0700

    create file

---
+++ src/components/DrinkForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class DrinkForm extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Create your drink:
+            </div>
+        )
+    }
+}
+
+export default DrinkForm
