ea7deb3f9c71c693af98efa213310f1593712270
Mitzi17 <m_estrada17@live.com>
2021-07-10 23:36:34 -0700

    pass in props to render drink attributes

--- src/components/DrinksList.js
+++ src/components/DrinksList.js
@@ -4,8 +4,20 @@ const DrinksList = props => {
     
     return(
         <div>
-            Drinks
+            <h2>Drinks</h2>
+            <div>
+                {props.drinks && props.drinks.map(drink =>
+                    <div key={drink.id}>
+                        <h3>Drink #: {props.drinks.indexOf(drink)}<br></br></h3>
+                            <li>Base: {drink.base}<br></br></li>
+                            <li>Flavor: {drink.flavor}<br></br></li>
+                            <li>Size: {drink.size}<br></br></li>
+                            <li>Add_on: {drink.add_on}</li>
+                    </div>
+                    )}
+            </div>
         </div>
+        
     )
 }
 
