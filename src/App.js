006719c55e8926680bdc928f1027555cea268a51
Mitzi17 <m_estrada17@live.com>
2021-07-19 12:08:55 -0700

    import NavMenu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import OrdersContainer from './containers/OrdersContainer'
+import { Route } from 'react-router-dom'
+import NavMenu from './components/Navbar';
 
 
 class App extends React.Component {
@@ -12,6 +14,8 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
+        <NavMenu />
+        HELLO WELCOME!!!
         <OrdersContainer/>
       </div>
       )
