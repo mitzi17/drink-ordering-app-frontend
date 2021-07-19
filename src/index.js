e44262dd49cde5e7f901f9924eac574ee66ac03e
Mitzi17 <m_estrada17@live.com>
2021-07-19 12:10:40 -0700

    import bootstrap.min.css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import orderReducer from './reducers/orderReducer'
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 
