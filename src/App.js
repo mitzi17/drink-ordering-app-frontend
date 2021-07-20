7a6a56bd4efa5bc9bbef9577449c28076f3c4a6f
Mitzi17 <m_estrada17@live.com>
2021-07-20 10:53:26 -0700

    import Homepage component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import OrdersContainer from './containers/OrdersContainer'
 import { Route } from 'react-router-dom'
 import NavMenu from './components/Navbar';
+import Homepage from './components/Homepage';
 
 
 class App extends React.Component {
@@ -15,7 +16,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <NavMenu />
-        HELLO WELCOME!!!
+        <Route exact path='/' component={Homepage} />
         <OrdersContainer/>
       </div>
       )
